d118855ed8e0b4b93dcb593ea06f6cd087eed307
Joseph Kinahan <kinahanj25@gmail.com>
2018-12-25 17:49:12 +0000

    again

--- wicked.java
+++ wicked.java
@@ -2,7 +2,7 @@ public class wicked  {
 	
 	public static void main (String [] args){
 
-		System.out.println("Yip I made it here.....now this");
+		System.out.println("Yip I made it here.....now this.....and now over here");
 }
 
 }
